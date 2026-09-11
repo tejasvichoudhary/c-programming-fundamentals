6dcce1c1aef49ac3b3001f289b38ac17b4f5ddf2
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-09-11 21:46:09 +0530

    learn the syntax

---
+++ practice_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// int main(){
+//     printf("My name is Prakash\n");
+//     printf("I am 25 years old\n");
+//     return 0;
+// }
+
+// int main(){
+//     printf("Success is the sum of small efforts repeated day in and day out");
+//     return 0;
+//         }
+
+// int main(){
+//     printf("Hello, C Learner!\n");
+//     printf("Welcome to the world of programming.\n");
+//     return 0;
+// }
+
+int main(){
+    printf("I am learning C programming.\n");
+    printf("It feels exciting.\n");
+    printf("This is just the beginning!\n");
+    return 0;
+}

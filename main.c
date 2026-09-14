0355ce70af1c14f8ec8b251e4488decdbf074821
tejasvichoudhary <tejasvichoudhary1@gmail.com>
2026-09-14 23:50:06 +0530

    basics

--- main.c
+++ main.c
@@ -33,3 +33,14 @@ int main(){
 
   return 0;
 }
+
+int main(){
+  int a = 5 , b = 10;
+  
+printf("the value of a + b is %d\n", a + b );
+printf("the value of a - b is %d\n", a - b );
+printf("the value of a * b is %d\n", a * b );
+printf("the value of a / b is %d\n", a / b );
+printf("the value of a %% b is %d\n", a % b );
+return 0;
+}
